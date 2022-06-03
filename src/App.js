ec9c36f9935017d243e6ec762eff77419e7c064f
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-03 23:34:23 +0900

    6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = () => {
+    setCounter((prev) => prev + 1)
+  }
+
+  console.log("Call an api!");
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Hello World"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
