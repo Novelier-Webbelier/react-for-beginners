1dc1bbc8d2b76481a04f1bdaaff2b9af880900b4
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-05 01:41:43 +0900

    7.6 Parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function Movie({ id, title, coverImg, summary, genres }) {
 	return (
 		<div key={id}>
-			<Link to="/movie">
+			<Link to={`/movie/${id}`}>
 				<h2>{title}</h2>
 			</Link>
 			<img src={coverImg} alt={title} />
@@ -24,7 +24,7 @@ Movie.propTypes = {
 	coverImg: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
 	summary: PropTypes.string.isRequired,
-	genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
-	return <h1>Detail of the Movie</h1>;
+	const { id } = useParams();
+	const [loading, setLoading] = useState(true);
+	const [movie, setMovie] = useState([]);
+
+	const getMovie = async () => {
+		const json = await (
+			await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+		).json();
+
+		setLoading(false);
+		setMovie(json.data.movie);
+	};
+
+	useEffect(() => {
+		getMovie();
+	}, []);
+
+	return (
+		<div>
+			<h1>{loading ? "Loading..." : `Detail of the ${movie.title}`}</h1>
+
+			<Movie
+				id={movie.id}
+				coverImg={movie.medium_cover_image}
+				title={movie.title}
+				summary={movie.description_full}
+				genres={movie.genres || ["Here is no Genres"]}
+			/>
+
+			<Link to="/">
+				<button>Back to Home</button>
+			</Link>
+		</div>
+	);
 }
 
 export default Detail;
