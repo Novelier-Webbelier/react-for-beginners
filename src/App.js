8ee3618c77d64d4982e43cfe8a6da7fcd352e2e8
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-04 17:13:35 +0900

    [Code Challenge] Complete! But, I combine the all components. because It is hard to pass the variable between Child Components and Child Components.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import {useEffect, useState} from "react";
-
-function SelectCoins({ coins }) {
-  return (
-    <select>
-      {coins.map((coin, index) => (
-        <option key={index}>
-          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-        </option>
-      ))}
-    </select>
-  )
-}
-
-function Loading() {
-  return (
-    <strong>Loading...</strong>
-  )
-}
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [userPrice, setUserPrice] = useState(100);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
-        setCoins(json)
+        setCoins(json);
         setLoading(false);
-      })
-  }, [])
+      });
+  }, []);
+
+  const onChangeInput = (event) => {
+    setUserPrice(event.target.value);
+  }
 
   return (
     <div>
       <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-      {loading ? <Loading /> : <SelectCoins coins={coins} />}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <input type={"number"} min={0} value={userPrice} onChange={onChangeInput} />
+          <ul>
+            {coins.map((coin, index) => {
+              if (userPrice >= coin.quotes.USD.price) {
+                return (
+                  <li key={index}>
+                    {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
